08bb2438508250fed0e584089ab014e359b855ec
Kashish-Garg123 <kashish20012000@gmail.com>
2023-10-18 15:41:09 +0530

    Metamask

--- src/App.js
+++ src/App.js
@@ -1,13 +1,83 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import ErrorMessage from "./components/ErrorMessage";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
+  const [error,setError]= useState();
+  const networks = {
+    polygon: {
+      chainId: `0x${Number(137).toString(16)}`,
+      chainName: "Polygon Mainnet",
+      nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+        decimals: 18
+      },
+      rpcUrls: ["https://polygon-rpc.com/"],
+      blockExplorerUrls: ["https://polygonscan.com/"]
+    },
+    bsc: {
+      chainId: `0x${Number(56).toString(16)}`,
+      chainName: "Binance Smart Chain Mainnet",
+      nativeCurrency: {
+        name: "Binance Chain Native Token",
+        symbol: "BNB",
+        decimals: 18
+      },
+      rpcUrls: [
+        "https://bsc-dataseed1.binance.org",
+        "https://bsc-dataseed2.binance.org",
+        "https://bsc-dataseed3.binance.org",
+        "https://bsc-dataseed4.binance.org",
+        "https://bsc-dataseed1.defibit.io",
+        "https://bsc-dataseed2.defibit.io",
+        "https://bsc-dataseed3.defibit.io",
+        "https://bsc-dataseed4.defibit.io",
+        "https://bsc-dataseed1.ninicoin.io",
+        "https://bsc-dataseed2.ninicoin.io",
+        "https://bsc-dataseed3.ninicoin.io",
+        "https://bsc-dataseed4.ninicoin.io",
+        "wss://bsc-ws-node.nariox.org"
+      ],
+      blockExplorerUrls: ["https://bscscan.com"]
+    }
+  };
+  
+  const changeNetwork = async ({ networkName, setError }) => {
+    try {
+      if (!window.ethereum) throw new Error("No crypto wallet found");
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            ...networks[networkName]
+          }
+        ]
+      });
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const handleNetworkSwitch = async(networkName)=> {
+    setError();
+    await changeNetwork({networkName,setError});
+  }
+  const networkchanged = (chainId) =>{
+    console.log({chainId});
+  }
 
   useEffect(() => {
     getCurrentWalletConnected();
-    addWalletListener();
   }, [walletAddress]);
+  // addWalletListener();
+  useEffect(()=>{
+    window.ethereum.on("chainChanged",networkchanged)
+    return () => {
+      window.ethereum.removeListener("chainchanged",networkchanged)
+    };
+  },[]);
 
   const connectWallet = async () => {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
@@ -47,13 +117,13 @@ function App() {
       console.log("Please install MetaMask");
     }
   };
-
+  
+  window.ethereum.on("accountsChanged", (accounts) => {
+    setWalletAddress(accounts[0]);
+    console.log(accounts[0]);
+  });
   const addWalletListener = async () => {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        setWalletAddress(accounts[0]);
-        console.log(accounts[0]);
-      });
     } else {
       /* MetaMask is not installed */
       setWalletAddress("");
@@ -72,7 +142,7 @@ function App() {
             <div className="navbar-end is-align-items-center">
               <button
                 className="button is-white connect-wallet"
-                onClick={connectWallet}
+                onClick={(e)=>connectWallet()}
               >
                 <span className="is-link has-text-weight-bold">
                   {walletAddress && walletAddress.length > 0
@@ -115,6 +185,19 @@ function App() {
               </article>
             </div>
           </div>
+        </div>
+        <div>
+        <button
+            onClick={() => handleNetworkSwitch("polygon")}
+          >
+            Switch to Polygon
+          </button>
+          <button
+            onClick={() => handleNetworkSwitch("bsc")}
+          >
+            Switch to BSC
+          </button>
+          <ErrorMessage message={error} />    
         </div>
       </section>
     </div>
